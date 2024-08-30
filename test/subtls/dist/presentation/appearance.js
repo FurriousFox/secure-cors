93ae66713a9a918c547b593063fb4f5b5c5dd5be
FurriousFox <klokhuiswertas@gmail.com>
2024-08-30 22:02:06 +0200

    remove test

--- test/subtls/dist/presentation/appearance.js
+++
@@ -1,7 +0,0 @@
-export var LogColours;
-(function (LogColours) {
-    LogColours["client"] = "#8cc";
-    LogColours["server"] = "#88c";
-    LogColours["header"] = "#c88";
-})(LogColours || (LogColours = {}));
-export const indentChars = '·· '; // careful: this has complex interactions with highlightBytes
--- test/subtls/src/presentation/appearance.ts
+++
@@ -1,8 +0,0 @@
-
-export enum LogColours {
-  client = '#8cc',
-  server = '#88c',
-  header = '#c88',
-}
-
-export const indentChars = '·· ';  // careful: this has complex interactions with highlightBytes
